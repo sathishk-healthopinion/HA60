39aa9b72ed913ea3fd6682119b8936fd6d366284
sathishk-healthopinion <30718292+sathishk-healthopinion@users.noreply.github.com>
2020-01-06 12:49:47 +0530

    Updates

---
+++ precache-manifest.bf75d042f349ef4d99c85e8e7304cc1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d329f6349cd9b9cdeb7edb241db10e7",
+    "url": "/HA60/index.html"
+  },
+  {
+    "revision": "e3c4c69defc12e22d95d",
+    "url": "/HA60/static/css/main.479db6ae.chunk.css"
+  },
+  {
+    "revision": "6eb720a72e1d71caea57",
+    "url": "/HA60/static/js/2.30b781cc.chunk.js"
+  },
+  {
+    "revision": "ff5c60e29370ae197a9604a5c4ebf6bc",
+    "url": "/HA60/static/js/2.30b781cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e3c4c69defc12e22d95d",
+    "url": "/HA60/static/js/main.e5fe98cb.chunk.js"
+  },
+  {
+    "revision": "699b23def0227eae16fd",
+    "url": "/HA60/static/js/runtime-main.e7d1eca1.js"
+  }
+]);
